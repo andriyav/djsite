ec5c317b955eccd993a024a84e1983ecd21ddf9f
andriyav@hotmail.com <andriyav@hotmail.com>
2022-05-24 01:02:08 +0300

    db table Category

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,20 +2,38 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from .models import *
 
-menu = ['Про сайт', 'Добавити пост  ', 'Зворотній звязок', 'Вхід']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
 
 def index(request):
     posts = Women.objects.all()
-    contest = {
+    contexst = {
         'posts': posts,
         'menu': menu,
         'title': 'Головна сторінка'
     }
 
-    return render(request, 'women/index.html', contest)
+    return render(request, 'women/index.html', contexst)
 
 def about(request):
     return render(request, 'women/about.html')
+
+def add_page(request):
+    return render(request, 'women/about.html')
+
+def contact(request):
+    return render(request, 'women/about.html')
+
+def login(request):
+    return render(request, 'women/about.html')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'page number {post_id}')
 
 def categories(request, cat):
     return HttpResponse(f"<h1> Category  {cat} </h1>")
