7c835d36f292094bec060aa6ce8bec65fab7263b
andriyav@hotmail.com <andriyav@hotmail.com>
2022-06-21 00:16:50 +0300

    DataMixin

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import AddPostForm
 from .models import *
+from .utils import *
 # menu = [{'title': "О сайте", 'url_name': 'about'},
 #         {'title': "Добавить статью", 'url_name': 'add_page'},
 #         {'title': "Обратная связь", 'url_name': 'contact'},
@@ -19,18 +20,17 @@ from .models import *
 #     }
 #
 #     return render(request, 'women/index.html', contexst)
-class WomenHome(ListView):
+class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
 
 
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Головна сторінка'
-        context['cat_selected'] = 0
-
-        return context
+        c_def = self.get_user_context(title=" Головна сторінка")
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
@@ -50,9 +50,10 @@ class WomenCategory(ListView):
         context['cat_selected'] = context['posts'][0].cat_id
         return context
 
-class AddPage(CreateView):
+class AddPage(LoginRequiredMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
+    login_url = '/admin/'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -94,8 +95,7 @@ class ShowPost(DetailView):
         return context
 
 
-# def show_post(request, cat_id):
-#     return HttpResponse(f'page number {cat_id}')
+
 
 
 def about(request):
